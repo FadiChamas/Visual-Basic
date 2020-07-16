1aec93bdf279f96f2fb8f2154f1b611a5d1d2799
Fadi Chamas <52497003+FadiChamas@users.noreply.github.com>
2020-07-16 13:30:59 -0400

    Add files via upload

---
+++ ChamasF_AlarmSystem/ChamasF_AlarmSystem/ChamasF_AlarmSystem/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmAlarmSystem
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.msktxtCode = New System.Windows.Forms.MaskedTextBox()
+		Me.btnOne = New System.Windows.Forms.Button()
+		Me.btnTwo = New System.Windows.Forms.Button()
+		Me.btnThree = New System.Windows.Forms.Button()
+		Me.btnFour = New System.Windows.Forms.Button()
+		Me.btnFive = New System.Windows.Forms.Button()
+		Me.btnSix = New System.Windows.Forms.Button()
+		Me.btnSeven = New System.Windows.Forms.Button()
+		Me.btnEight = New System.Windows.Forms.Button()
+		Me.btnNine = New System.Windows.Forms.Button()
+		Me.btnZero = New System.Windows.Forms.Button()
+		Me.btnEnter = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'msktxtCode
+		'
+		Me.msktxtCode.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.msktxtCode.Location = New System.Drawing.Point(18, 17)
+		Me.msktxtCode.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.msktxtCode.Mask = "00000"
+		Me.msktxtCode.Name = "msktxtCode"
+		Me.msktxtCode.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+		Me.msktxtCode.Size = New System.Drawing.Size(297, 27)
+		Me.msktxtCode.TabIndex = 0
+		Me.msktxtCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		'
+		'btnOne
+		'
+		Me.btnOne.AutoSize = True
+		Me.btnOne.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnOne.Location = New System.Drawing.Point(18, 56)
+		Me.btnOne.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnOne.Name = "btnOne"
+		Me.btnOne.Size = New System.Drawing.Size(90, 39)
+		Me.btnOne.TabIndex = 1
+		Me.btnOne.Text = "1"
+		Me.btnOne.UseVisualStyleBackColor = True
+		'
+		'btnTwo
+		'
+		Me.btnTwo.AutoSize = True
+		Me.btnTwo.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnTwo.Location = New System.Drawing.Point(119, 56)
+		Me.btnTwo.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnTwo.Name = "btnTwo"
+		Me.btnTwo.Size = New System.Drawing.Size(90, 39)
+		Me.btnTwo.TabIndex = 2
+		Me.btnTwo.Text = "2"
+		Me.btnTwo.UseVisualStyleBackColor = True
+		'
+		'btnThree
+		'
+		Me.btnThree.AutoSize = True
+		Me.btnThree.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnThree.Location = New System.Drawing.Point(225, 56)
+		Me.btnThree.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnThree.Name = "btnThree"
+		Me.btnThree.Size = New System.Drawing.Size(90, 39)
+		Me.btnThree.TabIndex = 3
+		Me.btnThree.Text = "3"
+		Me.btnThree.UseVisualStyleBackColor = True
+		'
+		'btnFour
+		'
+		Me.btnFour.AutoSize = True
+		Me.btnFour.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnFour.Location = New System.Drawing.Point(18, 103)
+		Me.btnFour.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnFour.Name = "btnFour"
+		Me.btnFour.Size = New System.Drawing.Size(90, 39)
+		Me.btnFour.TabIndex = 4
+		Me.btnFour.Text = "4"
+		Me.btnFour.UseVisualStyleBackColor = True
+		'
+		'btnFive
+		'
+		Me.btnFive.AutoSize = True
+		Me.btnFive.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnFive.Location = New System.Drawing.Point(119, 103)
+		Me.btnFive.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnFive.Name = "btnFive"
+		Me.btnFive.Size = New System.Drawing.Size(90, 39)
+		Me.btnFive.TabIndex = 5
+		Me.btnFive.Text = "5"
+		Me.btnFive.UseVisualStyleBackColor = True
+		'
+		'btnSix
+		'
+		Me.btnSix.AutoSize = True
+		Me.btnSix.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnSix.Location = New System.Drawing.Point(225, 103)
+		Me.btnSix.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnSix.Name = "btnSix"
+		Me.btnSix.Size = New System.Drawing.Size(90, 39)
+		Me.btnSix.TabIndex = 6
+		Me.btnSix.Text = "6"
+		Me.btnSix.UseVisualStyleBackColor = True
+		'
+		'btnSeven
+		'
+		Me.btnSeven.AutoSize = True
+		Me.btnSeven.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnSeven.Location = New System.Drawing.Point(18, 150)
+		Me.btnSeven.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnSeven.Name = "btnSeven"
+		Me.btnSeven.Size = New System.Drawing.Size(90, 39)
+		Me.btnSeven.TabIndex = 7
+		Me.btnSeven.Text = "7"
+		Me.btnSeven.UseVisualStyleBackColor = True
+		'
+		'btnEight
+		'
+		Me.btnEight.AutoSize = True
+		Me.btnEight.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnEight.Location = New System.Drawing.Point(119, 150)
+		Me.btnEight.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnEight.Name = "btnEight"
+		Me.btnEight.Size = New System.Drawing.Size(90, 39)
+		Me.btnEight.TabIndex = 8
+		Me.btnEight.Text = "8"
+		Me.btnEight.UseVisualStyleBackColor = True
+		'
+		'btnNine
+		'
+		Me.btnNine.AutoSize = True
+		Me.btnNine.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnNine.Location = New System.Drawing.Point(225, 150)
+		Me.btnNine.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnNine.Name = "btnNine"
+		Me.btnNine.Size = New System.Drawing.Size(90, 39)
+		Me.btnNine.TabIndex = 9
+		Me.btnNine.Text = "9"
+		Me.btnNine.UseVisualStyleBackColor = True
+		'
+		'btnZero
+		'
+		Me.btnZero.AutoSize = True
+		Me.btnZero.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnZero.Location = New System.Drawing.Point(18, 197)
+		Me.btnZero.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnZero.Name = "btnZero"
+		Me.btnZero.Size = New System.Drawing.Size(90, 39)
+		Me.btnZero.TabIndex = 10
+		Me.btnZero.Text = "0"
+		Me.btnZero.UseVisualStyleBackColor = True
+		'
+		'btnEnter
+		'
+		Me.btnEnter.AutoSize = True
+		Me.btnEnter.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnEnter.Location = New System.Drawing.Point(119, 197)
+		Me.btnEnter.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.btnEnter.Name = "btnEnter"
+		Me.btnEnter.Size = New System.Drawing.Size(196, 39)
+		Me.btnEnter.TabIndex = 11
+		Me.btnEnter.Text = "Enter"
+		Me.btnEnter.UseVisualStyleBackColor = True
+		'
+		'frmAlarmSystem
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 18.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+		Me.ClientSize = New System.Drawing.Size(348, 259)
+		Me.Controls.Add(Me.btnEnter)
+		Me.Controls.Add(Me.btnZero)
+		Me.Controls.Add(Me.btnNine)
+		Me.Controls.Add(Me.btnEight)
+		Me.Controls.Add(Me.btnSeven)
+		Me.Controls.Add(Me.btnSix)
+		Me.Controls.Add(Me.btnFive)
+		Me.Controls.Add(Me.btnFour)
+		Me.Controls.Add(Me.btnThree)
+		Me.Controls.Add(Me.btnTwo)
+		Me.Controls.Add(Me.btnOne)
+		Me.Controls.Add(Me.msktxtCode)
+		Me.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+		Me.Name = "frmAlarmSystem"
+		Me.Text = "Alarm System"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents msktxtCode As MaskedTextBox
+	Friend WithEvents btnOne As Button
+	Friend WithEvents btnTwo As Button
+	Friend WithEvents btnThree As Button
+	Friend WithEvents btnFour As Button
+	Friend WithEvents btnFive As Button
+	Friend WithEvents btnSix As Button
+	Friend WithEvents btnSeven As Button
+	Friend WithEvents btnEight As Button
+	Friend WithEvents btnNine As Button
+	Friend WithEvents btnZero As Button
+	Friend WithEvents btnEnter As Button
+End Class
---
+++ ChamasF_AlarmSystem/ChamasF_AlarmSystem/ChamasF_AlarmSystem/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class frmAlarmSystem
+	Private strCode As String
+	Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+		msktxtCode.Text = msktxtCode.Text & "1"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+		msktxtCode.Text = msktxtCode.Text & "2"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+		msktxtCode.Text = msktxtCode.Text & "3"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+		msktxtCode.Text = msktxtCode.Text & "4"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+		msktxtCode.Text = msktxtCode.Text & "5"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+		msktxtCode.Text = msktxtCode.Text & "6"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+		msktxtCode.Text = msktxtCode.Text & "7"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnEight_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+		msktxtCode.Text = msktxtCode.Text & "8"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+		msktxtCode.Text = msktxtCode.Text & "9"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnZero_Click(sender As Object, e As EventArgs) Handles btnZero.Click
+		msktxtCode.Text = msktxtCode.Text & "0"
+		strCode = msktxtCode.Text
+	End Sub
+	Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+		Call ValidCode(strCode)
+	End Sub
+	Private Function ValidCode(code As String) As Boolean
+		If code = 62498 Then
+			MessageBox.Show("ACCESS GRANTED", "Alarm System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+			Application.Exit()
+		Else
+			MessageBox.Show("ACCESS DENIED!", "Alarm System", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			msktxtCode.Text = ""
+		End If
+	End Function
+End Class
