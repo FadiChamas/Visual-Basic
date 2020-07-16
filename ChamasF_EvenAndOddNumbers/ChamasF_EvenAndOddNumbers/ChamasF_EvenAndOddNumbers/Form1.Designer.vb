688fa5a82ced1470273b6f12efd6634f3990646c
Fadi Chamas <52497003+FadiChamas@users.noreply.github.com>
2020-07-16 13:47:29 -0400

    Add files via upload

---
+++ ChamasF_EvenAndOddNumbers/ChamasF_EvenAndOddNumbers/ChamasF_EvenAndOddNumbers/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEvenAndOddNumbers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lstArray = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.AutoSize = True
+        Me.btnGenerate.Location = New System.Drawing.Point(12, 12)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(239, 26)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lstArray
+        '
+        Me.lstArray.FormattingEnabled = True
+        Me.lstArray.ItemHeight = 16
+        Me.lstArray.Location = New System.Drawing.Point(12, 44)
+        Me.lstArray.Name = "lstArray"
+        Me.lstArray.Size = New System.Drawing.Size(239, 356)
+        Me.lstArray.TabIndex = 1
+        '
+        'frmEvenAndOddNumbers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(263, 403)
+        Me.Controls.Add(Me.lstArray)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmEvenAndOddNumbers"
+        Me.Text = "Even And Odd Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents lstArray As ListBox
+End Class
---
+++ ChamasF_EvenAndOddNumbers/ChamasF_EvenAndOddNumbers/ChamasF_EvenAndOddNumbers/Form1.vb
@@ -0,0 +1,53 @@
+﻿'**************************************************************
+' PROGRAMME	: ARRAY Ex01 Even And Odd Numbers
+'  
+' OUTLINE : With the click of a button, this programme 
+'           generates an array Of random numbers, checks the 
+'           array for even And odd numbers, and displays which 
+'           values are even Or odd, all In one list box.
+' 
+' PROGRAMMER :	Fadi Chamas
+'
+' DATE	:	April 12th, 2019
+' **************************************************************
+Public Class frmEvenAndOddNumbers
+    Private iNum(9) As Integer
+    Private rand As New Random
+    Private strOutput As String = Nothing
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        FillArray()
+        lstArray.Items.Add("")
+        lstArray.Items.Add("EVEN NUMBERS IN ARRAY:")
+        EvenNumbers()
+        lstArray.Items.Add("")
+        lstArray.Items.Add("ODD NUMBERS IN ARRAY:")
+        OddNumbers()
+    End Sub
+    Private Sub FillArray()
+        For i = 0 To iNum.Length - 1
+            iNum(i) = rand.Next(0, 100)
+        Next
+        DisplayArray(iNum)
+    End Sub
+    Private Sub DisplayArray(iNum)
+        For j = 0 To iNum.Length - 1
+            lstArray.Items.Add(j & vbTab & iNum(j))
+        Next
+    End Sub
+    Private Sub EvenNumbers()
+        For k = 0 To iNum.Length - 1
+            Dim iRemainder As Integer = iNum(k) Mod 2
+            If iRemainder = 0 Then
+                lstArray.Items.Add(iNum(k))
+            End If
+        Next
+    End Sub
+    Private Sub OddNumbers()
+        For l = 0 To iNum.Length - 1
+            Dim iRemainder As Integer = iNum(l) Mod 2
+            If iRemainder = 1 Then
+                lstArray.Items.Add(iNum(l))
+            End If
+        Next
+    End Sub
+End Class
