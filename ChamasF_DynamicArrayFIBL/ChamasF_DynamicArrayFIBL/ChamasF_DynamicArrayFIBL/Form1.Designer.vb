cee66d4c5ca817dc41169ca125786c67e1d575d3
Fadi Chamas <52497003+FadiChamas@users.noreply.github.com>
2020-07-16 13:46:09 -0400

    Add files via upload

---
+++ ChamasF_DynamicArrayFIBL/ChamasF_DynamicArrayFIBL/ChamasF_DynamicArrayFIBL/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDynamicArrayFIBL
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.lblFIBL = New System.Windows.Forms.Label()
+        Me.lblFIBLValues = New System.Windows.Forms.Label()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOut
+        '
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.ItemHeight = 16
+        Me.lstOut.Location = New System.Drawing.Point(12, 12)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.Size = New System.Drawing.Size(269, 20)
+        Me.lstOut.TabIndex = 0
+        '
+        'lblFIBL
+        '
+        Me.lblFIBL.AutoSize = True
+        Me.lblFIBL.Location = New System.Drawing.Point(9, 44)
+        Me.lblFIBL.Name = "lblFIBL"
+        Me.lblFIBL.Size = New System.Drawing.Size(180, 16)
+        Me.lblFIBL.TabIndex = 1
+        Me.lblFIBL.Text = "First Increased by Last:"
+        '
+        'lblFIBLValues
+        '
+        Me.lblFIBLValues.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFIBLValues.Location = New System.Drawing.Point(12, 60)
+        Me.lblFIBLValues.Name = "lblFIBLValues"
+        Me.lblFIBLValues.Size = New System.Drawing.Size(269, 41)
+        Me.lblFIBLValues.TabIndex = 2
+        '
+        'btnShow
+        '
+        Me.btnShow.AutoSize = True
+        Me.btnShow.Location = New System.Drawing.Point(15, 114)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(76, 26)
+        Me.btnShow.TabIndex = 3
+        Me.btnShow.Text = "SHOW"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.AutoSize = True
+        Me.btnClear.Location = New System.Drawing.Point(113, 114)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(76, 26)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "CLEAR"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.AutoSize = True
+        Me.btnExit.Location = New System.Drawing.Point(205, 114)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(76, 26)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "EXIT"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmDynamicArrayFIBL
+        '
+        Me.AcceptButton = Me.btnShow
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aqua
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(295, 163)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnShow)
+        Me.Controls.Add(Me.lblFIBLValues)
+        Me.Controls.Add(Me.lblFIBL)
+        Me.Controls.Add(Me.lstOut)
+        Me.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "frmDynamicArrayFIBL"
+        Me.Text = "First Increased By Last"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstOut As ListBox
+    Friend WithEvents lblFIBL As Label
+    Friend WithEvents lblFIBLValues As Label
+    Friend WithEvents btnShow As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ ChamasF_DynamicArrayFIBL/ChamasF_DynamicArrayFIBL/ChamasF_DynamicArrayFIBL/Form1.vb
@@ -0,0 +1,68 @@
+﻿'**************************************************************
+' PROGRAMME	: ARRAY Ex03 Dynamic Array FIBL
+'  
+' OUTLINE : With the click of a button, this programme 
+'           generates an array of numbers inputted by the user, 
+'           displays the array in a listbox, checks the array for
+'           the first and last numbers, and adds them together for
+'           the number of numbers in the array.
+' 
+' PROGRAMMER :	Fadi Chamas
+'
+' DATE	:	April 18th, 2019
+' **************************************************************
+Public Class frmDynamicArrayFIBL
+    Private range As String = Nothing
+    Private clean As Boolean
+    Private num() As Integer
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim values As String = Nothing
+        Dim newfirst As Integer
+        Do
+            range = InputBox("Enter quantity of numbers (2-7) to input:", "INPUT")
+            If ErrorCheck(range) = True Then
+                ReDim num(CInt(range) - 1)
+                Exit Do
+            End If
+        Loop
+        For i = 0 To num.Length - 1
+            Do
+                num(i) = InputBox("Enter number " & i + 1 & ":")
+                ErrorCheck(num(i))
+                values &= num(i) & " "
+            Loop Until clean = True
+        Next
+        lstOut.Items.Add(values)
+        lblFIBLValues.Text = num(0) & " "
+        newfirst = num(0)
+        For j = 1 To num.Length - 1
+            newfirst = newfirst + num.Last
+            lblFIBLValues.Text &= CInt(newfirst) & " "
+        Next
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstOut.Items.Clear()
+        lblFIBLValues.Text = ""
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+    Private Function ErrorCheck(range As String)
+        clean = False
+        If range = Nothing Then
+            MessageBox.Show("You pressed Cancel - Goodbye!", "CANCEL", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Function
+        ElseIf Not IsNumeric(range) Then
+            MessageBox.Show("Use numbers!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Function
+        ElseIf range < 2 Then
+            MessageBox.Show("Not in 2 - 7 range!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Function
+        ElseIf range > 7 Then
+            MessageBox.Show("Not in 2 - 7 range!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Function
+        End If
+        clean = True
+        Return clean
+    End Function
+End Class
