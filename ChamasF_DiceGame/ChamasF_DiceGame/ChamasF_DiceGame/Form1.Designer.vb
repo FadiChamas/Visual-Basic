48671a979e9b3f53fdb58a29917170084c9ac17e
Fadi Chamas <52497003+FadiChamas@users.noreply.github.com>
2020-07-16 13:42:10 -0400

    Add files via upload

---
+++ ChamasF_DiceGame/ChamasF_DiceGame/ChamasF_DiceGame/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDiceGame
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.lblRisk = New System.Windows.Forms.Label()
+		Me.txtRisk = New System.Windows.Forms.TextBox()
+		Me.lblPoints = New System.Windows.Forms.Label()
+		Me.picCard = New System.Windows.Forms.PictureBox()
+		Me.picCard2 = New System.Windows.Forms.PictureBox()
+		Me.btnRoll = New System.Windows.Forms.Button()
+		CType(Me.picCard, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.picCard2, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'lblRisk
+		'
+		Me.lblRisk.AutoSize = True
+		Me.lblRisk.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblRisk.Location = New System.Drawing.Point(12, 21)
+		Me.lblRisk.Name = "lblRisk"
+		Me.lblRisk.Size = New System.Drawing.Size(114, 16)
+		Me.lblRisk.TabIndex = 1
+		Me.lblRisk.Text = "Points to Risk :"
+		'
+		'txtRisk
+		'
+		Me.txtRisk.Location = New System.Drawing.Point(163, 21)
+		Me.txtRisk.Name = "txtRisk"
+		Me.txtRisk.Size = New System.Drawing.Size(100, 20)
+		Me.txtRisk.TabIndex = 0
+		'
+		'lblPoints
+		'
+		Me.lblPoints.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lblPoints.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblPoints.Location = New System.Drawing.Point(12, 53)
+		Me.lblPoints.Name = "lblPoints"
+		Me.lblPoints.Size = New System.Drawing.Size(251, 19)
+		Me.lblPoints.TabIndex = 2
+		Me.lblPoints.Text = "Total Points : 1000"
+		'
+		'picCard
+		'
+		Me.picCard.Location = New System.Drawing.Point(15, 75)
+		Me.picCard.Name = "picCard"
+		Me.picCard.Size = New System.Drawing.Size(100, 100)
+		Me.picCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.picCard.TabIndex = 3
+		Me.picCard.TabStop = False
+		'
+		'picCard2
+		'
+		Me.picCard2.Location = New System.Drawing.Point(163, 75)
+		Me.picCard2.Name = "picCard2"
+		Me.picCard2.Size = New System.Drawing.Size(100, 100)
+		Me.picCard2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.picCard2.TabIndex = 4
+		Me.picCard2.TabStop = False
+		'
+		'btnRoll
+		'
+		Me.btnRoll.AutoSize = True
+		Me.btnRoll.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnRoll.Location = New System.Drawing.Point(81, 189)
+		Me.btnRoll.Name = "btnRoll"
+		Me.btnRoll.Size = New System.Drawing.Size(121, 26)
+		Me.btnRoll.TabIndex = 3
+		Me.btnRoll.Text = "Roll Dice"
+		Me.btnRoll.UseVisualStyleBackColor = True
+		'
+		'frmDiceGame
+		'
+		Me.AcceptButton = Me.btnRoll
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+		Me.ClientSize = New System.Drawing.Size(282, 229)
+		Me.Controls.Add(Me.btnRoll)
+		Me.Controls.Add(Me.picCard2)
+		Me.Controls.Add(Me.picCard)
+		Me.Controls.Add(Me.lblPoints)
+		Me.Controls.Add(Me.txtRisk)
+		Me.Controls.Add(Me.lblRisk)
+		Me.Name = "frmDiceGame"
+		Me.Text = "Dice Game"
+		CType(Me.picCard, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.picCard2, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents lblRisk As Label
+	Friend WithEvents txtRisk As TextBox
+	Friend WithEvents lblPoints As Label
+	Friend WithEvents picCard As PictureBox
+	Friend WithEvents picCard2 As PictureBox
+	Friend WithEvents btnRoll As Button
+End Class
---
+++ ChamasF_DiceGame/ChamasF_DiceGame/ChamasF_DiceGame/Form1.vb
@@ -0,0 +1,97 @@
+﻿'*******************************************************
+' PROGRAMME	: ASSN01 ColourMatch
+'  
+' OUTLINE : This programme asks the user to wager 
+'           "points" and to roll 2 die. If the total
+'           value of the die is even, they will lose their
+'           wager, if the total value is odd, they will
+'           double it.
+' 
+' PROGRAMMER :	Fadi Chamas
+'
+'  DATE	:	March 23rd, 2019
+' *****************************************************
+Public Class frmDiceGame
+	Private iRisk As Integer = 1000
+	Private iRisk2, rand1, rand2, dice As Integer
+	Private strRisk As String
+	Private rand As New Random
+	' procedure DicePics generates the pictures for the pictureboxes and determines the total value of the dice
+	Private Sub DicePics(picture As PictureBox, number As Integer)
+		Select Case number
+			Case 1
+				picture.Image = My.Resources.Dice_1
+				dice += 1
+			Case 2
+				picture.Image = My.Resources.Dice_2
+				dice += 2
+			Case 3
+				picture.Image = My.Resources.Dice_3
+				dice += 3
+			Case 4
+				picture.Image = My.Resources.Dice_4
+				dice += 4
+			Case 5
+				picture.Image = My.Resources.Dice_5
+				dice += 5
+			Case 6
+				picture.Image = My.Resources.Dice_6
+				dice += 6
+		End Select
+	End Sub
+	Private Sub btnRoll_Click(sender As Object, e As EventArgs) Handles btnRoll.Click
+		strRisk = txtRisk.Text
+		If Not IsNumeric(txtRisk.Text) Then
+			MessageBox.Show("Invalid Input!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			txtRisk.Clear()
+			txtRisk.Focus()
+			Exit Sub
+		End If
+		iRisk2 = strRisk
+		If iRisk2 <= 0 Then
+			MessageBox.Show("Cannot risk this amount!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			txtRisk.Clear()
+			txtRisk.Focus()
+			Exit Sub
+		ElseIf iRisk2 > iRisk Then
+			MessageBox.Show("Cannot risk this amount!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+			txtRisk.Clear()
+			txtRisk.Focus()
+			Exit Sub
+		End If
+		rand1 = rand.Next(1, 7)
+		rand2 = rand.Next(1, 7)
+		DicePics(picCard, rand1)
+		DicePics(picCard2, rand2)
+		Select Case dice
+			Case 2
+				iRisk -= iRisk2
+			Case 3
+				iRisk += iRisk2
+			Case 4
+				iRisk -= iRisk2
+			Case 5
+				iRisk += iRisk2
+			Case 6
+				iRisk -= iRisk2
+			Case 7
+				iRisk += iRisk2
+			Case 8
+				iRisk -= iRisk2
+			Case 9
+				iRisk += iRisk2
+			Case 10
+				iRisk -= iRisk2
+			Case 11
+				iRisk += iRisk2
+			Case 12
+				iRisk -= iRisk2
+		End Select
+		If iRisk = 0 Then
+			MessageBox.Show("Back on the poverty line you go.", "You're broke!", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+			Application.Exit()
+		End If
+		lblPoints.Text = "Total Points : " & iRisk
+		dice = 0
+	End Sub
+End Class
