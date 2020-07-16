ff1828108a7873ddeb4f174c736de8e09070edaf
Fadi Chamas <52497003+FadiChamas@users.noreply.github.com>
2020-07-16 13:32:46 -0400

    Add files via upload

---
+++ ChamasF_ArrayofLabels/ChamasF_ArrayOfLabels/ChamasF_ArrayOfLabels/Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArrayofLabels
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOne = New System.Windows.Forms.Label()
+        Me.lblTwo = New System.Windows.Forms.Label()
+        Me.lblSix = New System.Windows.Forms.Label()
+        Me.lblSeven = New System.Windows.Forms.Label()
+        Me.lblTen = New System.Windows.Forms.Label()
+        Me.lblFive = New System.Windows.Forms.Label()
+        Me.lblEight = New System.Windows.Forms.Label()
+        Me.lblNine = New System.Windows.Forms.Label()
+        Me.lblFour = New System.Windows.Forms.Label()
+        Me.lblThree = New System.Windows.Forms.Label()
+        Me.lblEighteen = New System.Windows.Forms.Label()
+        Me.lblNineteen = New System.Windows.Forms.Label()
+        Me.lblFourteen = New System.Windows.Forms.Label()
+        Me.lblThirteen = New System.Windows.Forms.Label()
+        Me.lblTwenty = New System.Windows.Forms.Label()
+        Me.lblFifteen = New System.Windows.Forms.Label()
+        Me.lblSixteen = New System.Windows.Forms.Label()
+        Me.lblSeventeen = New System.Windows.Forms.Label()
+        Me.lblTwelve = New System.Windows.Forms.Label()
+        Me.lblEleven = New System.Windows.Forms.Label()
+        Me.lblTwentyFour = New System.Windows.Forms.Label()
+        Me.lblTwentyThree = New System.Windows.Forms.Label()
+        Me.lblTwentyFive = New System.Windows.Forms.Label()
+        Me.lblTwentyTwo = New System.Windows.Forms.Label()
+        Me.lblTwentyOne = New System.Windows.Forms.Label()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblOne
+        '
+        Me.lblOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOne.Location = New System.Drawing.Point(3, 9)
+        Me.lblOne.Name = "lblOne"
+        Me.lblOne.Size = New System.Drawing.Size(36, 41)
+        Me.lblOne.TabIndex = 0
+        Me.lblOne.Text = "1"
+        Me.lblOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwo
+        '
+        Me.lblTwo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwo.Location = New System.Drawing.Point(54, 9)
+        Me.lblTwo.Name = "lblTwo"
+        Me.lblTwo.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwo.TabIndex = 1
+        Me.lblTwo.Text = "1"
+        Me.lblTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSix
+        '
+        Me.lblSix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSix.Location = New System.Drawing.Point(3, 50)
+        Me.lblSix.Name = "lblSix"
+        Me.lblSix.Size = New System.Drawing.Size(36, 45)
+        Me.lblSix.TabIndex = 5
+        Me.lblSix.Text = "1"
+        Me.lblSix.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSeven
+        '
+        Me.lblSeven.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSeven.Location = New System.Drawing.Point(54, 50)
+        Me.lblSeven.Name = "lblSeven"
+        Me.lblSeven.Size = New System.Drawing.Size(36, 45)
+        Me.lblSeven.TabIndex = 4
+        Me.lblSeven.Text = "1"
+        Me.lblSeven.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTen
+        '
+        Me.lblTen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTen.Location = New System.Drawing.Point(237, 50)
+        Me.lblTen.Name = "lblTen"
+        Me.lblTen.Size = New System.Drawing.Size(36, 45)
+        Me.lblTen.TabIndex = 9
+        Me.lblTen.Text = "1"
+        Me.lblTen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFive
+        '
+        Me.lblFive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFive.Location = New System.Drawing.Point(237, 9)
+        Me.lblFive.Name = "lblFive"
+        Me.lblFive.Size = New System.Drawing.Size(36, 45)
+        Me.lblFive.TabIndex = 8
+        Me.lblFive.Text = "1"
+        Me.lblFive.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEight
+        '
+        Me.lblEight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEight.Location = New System.Drawing.Point(115, 50)
+        Me.lblEight.Name = "lblEight"
+        Me.lblEight.Size = New System.Drawing.Size(36, 45)
+        Me.lblEight.TabIndex = 25
+        Me.lblEight.Text = "1"
+        Me.lblEight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNine
+        '
+        Me.lblNine.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNine.Location = New System.Drawing.Point(178, 50)
+        Me.lblNine.Name = "lblNine"
+        Me.lblNine.Size = New System.Drawing.Size(36, 45)
+        Me.lblNine.TabIndex = 24
+        Me.lblNine.Text = "1"
+        Me.lblNine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFour
+        '
+        Me.lblFour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFour.Location = New System.Drawing.Point(178, 9)
+        Me.lblFour.Name = "lblFour"
+        Me.lblFour.Size = New System.Drawing.Size(36, 45)
+        Me.lblFour.TabIndex = 17
+        Me.lblFour.Text = "1"
+        Me.lblFour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblThree
+        '
+        Me.lblThree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblThree.Location = New System.Drawing.Point(115, 9)
+        Me.lblThree.Name = "lblThree"
+        Me.lblThree.Size = New System.Drawing.Size(36, 45)
+        Me.lblThree.TabIndex = 16
+        Me.lblThree.Text = "1"
+        Me.lblThree.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEighteen
+        '
+        Me.lblEighteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEighteen.Location = New System.Drawing.Point(115, 140)
+        Me.lblEighteen.Name = "lblEighteen"
+        Me.lblEighteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblEighteen.TabIndex = 35
+        Me.lblEighteen.Text = "1"
+        Me.lblEighteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNineteen
+        '
+        Me.lblNineteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNineteen.Location = New System.Drawing.Point(178, 140)
+        Me.lblNineteen.Name = "lblNineteen"
+        Me.lblNineteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblNineteen.TabIndex = 34
+        Me.lblNineteen.Text = "1"
+        Me.lblNineteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFourteen
+        '
+        Me.lblFourteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFourteen.Location = New System.Drawing.Point(178, 95)
+        Me.lblFourteen.Name = "lblFourteen"
+        Me.lblFourteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblFourteen.TabIndex = 33
+        Me.lblFourteen.Text = "1"
+        Me.lblFourteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblThirteen
+        '
+        Me.lblThirteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblThirteen.Location = New System.Drawing.Point(115, 95)
+        Me.lblThirteen.Name = "lblThirteen"
+        Me.lblThirteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblThirteen.TabIndex = 32
+        Me.lblThirteen.Text = "1"
+        Me.lblThirteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwenty
+        '
+        Me.lblTwenty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwenty.Location = New System.Drawing.Point(237, 140)
+        Me.lblTwenty.Name = "lblTwenty"
+        Me.lblTwenty.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwenty.TabIndex = 31
+        Me.lblTwenty.Text = "1"
+        Me.lblTwenty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFifteen
+        '
+        Me.lblFifteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFifteen.Location = New System.Drawing.Point(237, 95)
+        Me.lblFifteen.Name = "lblFifteen"
+        Me.lblFifteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblFifteen.TabIndex = 30
+        Me.lblFifteen.Text = "1"
+        Me.lblFifteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSixteen
+        '
+        Me.lblSixteen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSixteen.Location = New System.Drawing.Point(3, 140)
+        Me.lblSixteen.Name = "lblSixteen"
+        Me.lblSixteen.Size = New System.Drawing.Size(36, 45)
+        Me.lblSixteen.TabIndex = 29
+        Me.lblSixteen.Text = "1"
+        Me.lblSixteen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSeventeen
+        '
+        Me.lblSeventeen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSeventeen.Location = New System.Drawing.Point(54, 140)
+        Me.lblSeventeen.Name = "lblSeventeen"
+        Me.lblSeventeen.Size = New System.Drawing.Size(36, 45)
+        Me.lblSeventeen.TabIndex = 28
+        Me.lblSeventeen.Text = "1"
+        Me.lblSeventeen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwelve
+        '
+        Me.lblTwelve.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwelve.Location = New System.Drawing.Point(54, 95)
+        Me.lblTwelve.Name = "lblTwelve"
+        Me.lblTwelve.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwelve.TabIndex = 27
+        Me.lblTwelve.Text = "1"
+        Me.lblTwelve.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEleven
+        '
+        Me.lblEleven.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEleven.Location = New System.Drawing.Point(3, 95)
+        Me.lblEleven.Name = "lblEleven"
+        Me.lblEleven.Size = New System.Drawing.Size(36, 45)
+        Me.lblEleven.TabIndex = 26
+        Me.lblEleven.Text = "1"
+        Me.lblEleven.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwentyFour
+        '
+        Me.lblTwentyFour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwentyFour.Location = New System.Drawing.Point(178, 185)
+        Me.lblTwentyFour.Name = "lblTwentyFour"
+        Me.lblTwentyFour.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwentyFour.TabIndex = 43
+        Me.lblTwentyFour.Text = "1"
+        Me.lblTwentyFour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwentyThree
+        '
+        Me.lblTwentyThree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwentyThree.Location = New System.Drawing.Point(115, 185)
+        Me.lblTwentyThree.Name = "lblTwentyThree"
+        Me.lblTwentyThree.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwentyThree.TabIndex = 42
+        Me.lblTwentyThree.Text = "1"
+        Me.lblTwentyThree.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwentyFive
+        '
+        Me.lblTwentyFive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwentyFive.Location = New System.Drawing.Point(237, 185)
+        Me.lblTwentyFive.Name = "lblTwentyFive"
+        Me.lblTwentyFive.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwentyFive.TabIndex = 40
+        Me.lblTwentyFive.Text = "1"
+        Me.lblTwentyFive.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwentyTwo
+        '
+        Me.lblTwentyTwo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwentyTwo.Location = New System.Drawing.Point(54, 185)
+        Me.lblTwentyTwo.Name = "lblTwentyTwo"
+        Me.lblTwentyTwo.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwentyTwo.TabIndex = 37
+        Me.lblTwentyTwo.Text = "1"
+        Me.lblTwentyTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTwentyOne
+        '
+        Me.lblTwentyOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTwentyOne.Location = New System.Drawing.Point(3, 185)
+        Me.lblTwentyOne.Name = "lblTwentyOne"
+        Me.lblTwentyOne.Size = New System.Drawing.Size(36, 45)
+        Me.lblTwentyOne.TabIndex = 36
+        Me.lblTwentyOne.Text = "1"
+        Me.lblTwentyOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.AutoSize = True
+        Me.btnGenerate.Location = New System.Drawing.Point(6, 248)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(170, 28)
+        Me.btnGenerate.TabIndex = 44
+        Me.btnGenerate.Text = "Generate New"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.AutoSize = True
+        Me.btnExit.Location = New System.Drawing.Point(181, 248)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 28)
+        Me.btnExit.TabIndex = 45
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lstOut
+        '
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.ItemHeight = 18
+        Me.lstOut.Location = New System.Drawing.Point(279, 9)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.Size = New System.Drawing.Size(192, 256)
+        Me.lstOut.TabIndex = 46
+        '
+        'frmArrayofLabels
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aqua
+        Me.ClientSize = New System.Drawing.Size(488, 280)
+        Me.Controls.Add(Me.lstOut)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.lblTwentyFour)
+        Me.Controls.Add(Me.lblTwentyThree)
+        Me.Controls.Add(Me.lblTwentyFive)
+        Me.Controls.Add(Me.lblTwentyTwo)
+        Me.Controls.Add(Me.lblTwentyOne)
+        Me.Controls.Add(Me.lblEighteen)
+        Me.Controls.Add(Me.lblNineteen)
+        Me.Controls.Add(Me.lblFourteen)
+        Me.Controls.Add(Me.lblThirteen)
+        Me.Controls.Add(Me.lblTwenty)
+        Me.Controls.Add(Me.lblFifteen)
+        Me.Controls.Add(Me.lblSixteen)
+        Me.Controls.Add(Me.lblSeventeen)
+        Me.Controls.Add(Me.lblTwelve)
+        Me.Controls.Add(Me.lblEleven)
+        Me.Controls.Add(Me.lblEight)
+        Me.Controls.Add(Me.lblNine)
+        Me.Controls.Add(Me.lblFour)
+        Me.Controls.Add(Me.lblThree)
+        Me.Controls.Add(Me.lblTen)
+        Me.Controls.Add(Me.lblFive)
+        Me.Controls.Add(Me.lblSix)
+        Me.Controls.Add(Me.lblSeven)
+        Me.Controls.Add(Me.lblTwo)
+        Me.Controls.Add(Me.lblOne)
+        Me.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.Name = "frmArrayofLabels"
+        Me.Text = "Array of Labels"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblOne As Label
+    Friend WithEvents lblTwo As Label
+    Friend WithEvents lblSix As Label
+    Friend WithEvents lblSeven As Label
+    Friend WithEvents lblTen As Label
+    Friend WithEvents lblFive As Label
+    Friend WithEvents lblEight As Label
+    Friend WithEvents lblNine As Label
+    Friend WithEvents lblFour As Label
+    Friend WithEvents lblThree As Label
+    Friend WithEvents lblEighteen As Label
+    Friend WithEvents lblNineteen As Label
+    Friend WithEvents lblFourteen As Label
+    Friend WithEvents lblThirteen As Label
+    Friend WithEvents lblTwenty As Label
+    Friend WithEvents lblFifteen As Label
+    Friend WithEvents lblSixteen As Label
+    Friend WithEvents lblSeventeen As Label
+    Friend WithEvents lblTwelve As Label
+    Friend WithEvents lblEleven As Label
+    Friend WithEvents lblTwentyFour As Label
+    Friend WithEvents lblTwentyThree As Label
+    Friend WithEvents lblTwentyFive As Label
+    Friend WithEvents lblTwentyTwo As Label
+    Friend WithEvents lblTwentyOne As Label
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lstOut As ListBox
+End Class
---
+++ ChamasF_ArrayofLabels/ChamasF_ArrayOfLabels/ChamasF_ArrayOfLabels/Form1.vb
@@ -0,0 +1,77 @@
+﻿'**************************************************************
+' PROGRAMME	: Arrays Ex04 Array of Labels
+'  
+' OUTLINE : This programme allows the user to select a button 					
+'			which generates random numbers between 1 and 9 which
+'           changes the backcolour of the labels, while also
+'           informing the user how many times each number is
+'           generated.
+'
+' PROGRAMMER :	Fadi Chamas
+'
+' DATE	:	April 26th, 2019
+' **************************************************************
+Public Class frmArrayofLabels
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim iLbl() As Label = {lblOne, lblTwo, lblThree, lblFour, lblFive, lblSix, lblSeven, lblEight, lblNine,
+                               lblTen, lblEleven, lblTwelve, lblThirteen, lblFourteen, lblFifteen, lblSixteen,
+                               lblSeventeen, lblEighteen, lblNineteen, lblTwenty, lblTwentyOne, lblTwentyTwo,
+                               lblTwentyThree, lblTwentyFour, lblTwentyFive}
+        Dim rand As New Random
+        For i As Integer = 0 To iLbl.Length - 1
+            iLbl(i).Text = rand.Next(1, 10)
+            Select Case iLbl(i).Text
+                Case 1
+                    iLbl(i).BackColor = Color.Red
+                Case 2
+                    iLbl(i).BackColor = Color.Salmon
+                Case 3
+                    iLbl(i).BackColor = Color.LightGray
+                Case 4
+                    iLbl(i).BackColor = Color.Yellow
+                Case 5
+                    iLbl(i).BackColor = Color.Khaki
+                Case 6
+                    iLbl(i).BackColor = Color.LimeGreen
+                Case 7
+                    iLbl(i).BackColor = Color.Orange
+                Case 8
+                    iLbl(i).BackColor = Color.LightBlue
+                Case 9
+                    iLbl(i).BackColor = Color.LightPink
+            End Select
+        Next
+        CountNumbers(iLbl)
+    End Sub
+    Private Sub CountNumbers(iLbl As Label())
+        Dim times(8) As Integer
+        For i As Integer = 0 To iLbl.Length - 1
+            Select Case iLbl(i).Text
+                Case 1
+                    times(0) += 1
+                Case 2
+                    times(1) += 1
+                Case 3
+                    times(2) += 1
+                Case 4
+                    times(3) += 1
+                Case 5
+                    times(4) += 1
+                Case 6
+                    times(5) += 1
+                Case 7
+                    times(6) += 1
+                Case 8
+                    times(7) += 1
+                Case 9
+                    times(8) += 1
+            End Select
+        Next
+        For i = 0 To times.Length - 1
+            lstOut.Items.Add(i + 1 & " is repeated " & times(i) & " times.")
+        Next
+    End Sub
+End Class
